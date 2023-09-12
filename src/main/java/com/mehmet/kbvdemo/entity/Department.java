4fdc830c072814f38dfaa11c8c101585a2cc8268
mehmetkirimli <mehmet.kirimli@vbt.com.tr>
2023-09-12 15:31:06 +0300

    fix second entity Unit

--- src/main/java/com/mehmet/kbvdemo/entity/Department.java
+++ src/main/java/com/mehmet/kbvdemo/entity/Department.java
@@ -3,9 +3,12 @@ package com.mehmet.kbvdemo.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import java.io.Serializable;
@@ -40,6 +43,11 @@ public class Department implements Serializable {
 
   @Column(name = "DEPARTMENT_GROUP")
   private Long departmentGroup;
+
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "unit_id")
+  private Unit unit;
 
   public String toString(){
     return "Department {" +
